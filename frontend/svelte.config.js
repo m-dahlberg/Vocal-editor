255f4cbeedc018ad2577fe194e61a721e6134920
Magnus <magnus.dahlberg@ymail.com>
2026-03-06 11:16:43 +0100

    Fix so that it runs in Docker Compose

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,7 +3,10 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ bodySizeLimit: 100 * 1024 * 1024 }),
+		csrf: {
+			checkOrigin: false
+		}
 	}
 };
 
